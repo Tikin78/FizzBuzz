eeb2f23547f99f324f481798001ed0d98dca0ce8
nauczyciel <tikin78@gmail.com>
2023-01-25 22:08:45 +0100

    Add plik FizzBuzzy

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class FizzBuzz
+    {
+        
+        
+        public void  Info()
+
+        {
+            Console.WriteLine("FizzBuzzy");
+        }
+    }
+
+}
+
+
+
+
+
+
+
+    
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę: ");
+            
+            while (true)
+            {
+                var liczba = GetLiczba();
+
+                if (liczba % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+
+                }
+
+                else if (liczba % 5 == 0)
+                {
+
+                    Console.WriteLine("Buzz");
+                }
+                else
+
+                    //FizzBuzz.Info();
+                   Console.WriteLine("FizzBuzzy");
+                   Console.WriteLine("Podaj następną liczbę. Jeżeli chcesz wyjść z aplikacji wpisz 't'.");
+                
+            }
+           
+        }
+
+        private static int GetLiczba()
+        {
+
+            while (true)
+            {
+                var val = Console.ReadLine();
+                if (val.ToUpper() == "T")
+                    Environment.Exit(0);
+
+
+
+                if (!int.TryParse(val, out int liczba))
+
+                {
+                    Console.WriteLine("Podałeś nie prawidłowe dane. Spróbój ponownie.");
+                    continue;
+                }
+                return liczba;
+
+            }
+        }
+    }
+}
+
